a398e557e0b7a1a390039fa079946d0a5181890d
Abreto Fu <m@abreto.net>
2018-08-05 02:33:51 +0800

    test with KaTex

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,18 @@
 module.exports = {
     title: 'UESTC_Jungle',
     description: 'Welcome to `WRONG ANSWER` world.',
+
+    head: [
+        // ['script', { type: 'text/javascript', src: 'https://cdn.bootcss.com/mathjax/2.7.4/latest.js', async: true }],
+        ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/KaTeX/0.10.0-alpha/katex.min.css' }],
+        ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/github-markdown-css/2.10.0/github-markdown.min.css' }],
+    ],
+
     markdown: {
         lineNumbers: true,
+        config: md => {
+            // md.use(require('markdown-it-mathjax')());
+            md.use(require('markdown-it-katex'));
+        },
     },
 };
--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 Welcome to `WRONG ANSWER` world.
 
+Inline equation: $a^2 = 1$.
+
+Test with
+
+$$
+S = \sum_{i=1}^k {a_i} + \int_a^b {f(x;\theta)\:\mathrm{d}x}
+$$
+
+... and other text.
+
 ```cpp{12}
 class ac_machine
 {

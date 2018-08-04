bbff9c3d2af436cb757a9139f3cbdd2aed8c3ad6
Abreto Fu <m@abreto.net>
2018-08-05 03:04:26 +0800

    have a try

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
 
     head: [
-        ['script', { type: 'text/javascript', src: 'https://cdn.bootcss.com/mathjax/2.7.4/latest.js', async: true }],
+        ['script', { type: 'text/javascript', src: 'https://cdn.bootcss.com/mathjax/2.7.4/latest.js?config=TeX-MML-AM_CHTML', async: true }],
         //['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/KaTeX/0.10.0-alpha/katex.min.css' }],
         //['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/github-markdown-css/2.10.0/github-markdown.min.css' }],
     ],
@@ -19,7 +19,7 @@ module.exports = {
     markdown: {
         lineNumbers: true,
         config: md => {
-            md.use(require('markdown-it-mathjax')());
+            //md.use(require('markdown-it-mathjax')());
             //md.use(require('markdown-it-katex'));
         },
     },

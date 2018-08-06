cae836fefd2bf883b9c0f6cabbb32ed504fce219
Abreto Fu <m@abreto.net>
2018-08-07 03:53:54 +0800

    add components

---
+++ .vuepress/components/training-trending.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="training-treding-container" ref="training-treding-container">
+    <training-chart />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'training-trending',
+  methods: {
+    enhance() {
+      const container = this.$refs['training-treding-container']
+      const clientWidth = document.documentElement.clientWidth
+
+      const canvsWidth = Math.min(clientWidth, 740)
+
+      container.style.width = `${canvsWidth}px`
+    }
+  },
+  mounted() {
+    this.enhance()
+  }
+}
+</script>
